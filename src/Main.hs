ddb13879c50989ed473bc6c5cffb116a99d739ff
mt <mt@windows>
2015-11-27 02:54:47 +0900

    修正中

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ import qualified Codec.Picture as Picture
 import qualified Codec.Picture.Types as Picture
 import qualified Codec.Picture.RGBA8 as Picture (fromDynamicImage)
 
+import Control.Monad (mplus)
 import Control.Monad.Primitive (PrimState)
 
 import Data.Array.IArray (Array, (!))
@@ -21,7 +22,9 @@ import System.FilePath.Find ((==?), (&&?))
 import qualified System.FilePath.Find as FManip
 
 main :: IO ()
-main = do
+main = putStrLn "hello"
+{-
+ do
     imgs <- loadFiles =<< listFilePaths ".png" "tmp"
     let sizes = Array.amap (Picture.dynamicMap (\x -> (Picture.imageWidth x, Picture.imageHeight x))) imgs
     let textureSize = (2048, 2048)
@@ -31,6 +34,7 @@ main = do
     print locations
     print pages
     writeTexture "test.png" textureSize (head pages) imgs
+-}
 
 
 listFilePaths :: String -> FilePath -> IO [FilePath]
@@ -45,57 +49,55 @@ loadFiles filepaths
     =<< mapM Picture.readImage filepaths
 
 
-data Location a = Location
-    { page :: Int
-    , position :: (Int, Int)
-    , rotated :: Bool
-    , value :: a
+data Rect a = Rect
+    { position :: (Int, Int)
+    , size :: (Int, Int)
+    , element :: Maybe (a, Rect a, Rect a)
     } deriving (Show, Eq)
 
-pack
+packImages
     :: (Int, Int)
     -> Array Int (Int, Int)
-    -> [Location Int]
-pack textureSize =
-    third . List.foldl' packOne (0, Map.empty, []) . take 10 . Array.assocs
+    -> [Rect Int]
+packImages textureSize =
+    List.foldl' pack [] . Array.assocs
 
     where
-    packOne (page, rects, locations) (index, imageSize) =
-        case Map.lookupGE imageSize rects of
-            Just (key, rect : tail) ->
-                let (loc, rest) = locate rect (index, imageSize)
-                    m' = insertsMap rest
-                       . Map.update (const $ if null tail then Nothing else Just tail) key 
-                       $ rects
-                    m'' = trace ("lookupGE: " ++ show imageSize ++ " <= " ++ show key) (insertsMap rest m')
-                in (page, m'', loc : locations)
+    pack :: [Rect Int] -> (Int, (Int, Int)) -> [Rect Int]
+    pack rects a =
+        rect (0, 0) textureSize a : rects `Maybe.fromMaybe` tryPack a rects
 
-            _ ->
-                let (loc, rest) = locate (newTexture (page + 1)) (index, imageSize)
-                in (page + 1, insertsMap rest rects, loc : locations)
+    rect (x, y) (rw, rh) (index, (w, h)) =
+        let childR = Rect (x + w, y) (rw - w, h) Nothing
+            childB = Rect (x, y + h) (w, rh - h) Nothing
+        in Rect (x, y) (rw, rh) (Just (index, childR, childB))
 
-    locate rect (index, imageSize) =
-        let p = page rect
-            (imageWidth, imageHeight) = imageSize
-            (rectWidth, rectHeight) = value rect
-            (x, y) = position rect
-            loc = Location p (x, y) False index
-            right = if rectWidth > imageWidth && rectHeight > imageHeight
-                        then Just $ Location p (x + imageWidth, y) False (rectWidth - imageWidth, imageHeight)
-                        else Nothing
-            bottom = if rectHeight > imageHeight
-                        then Just $ Location p (x, y + imageHeight) False (rectWidth, rectHeight - imageHeight)
-                        else Nothing
-        in (loc, Maybe.catMaybes [right, bottom])
+    tryPack a [] = Nothing
 
-    newTexture p = Location p (0, 0) False textureSize
+    tryPack a (r : rs) =
+        case tryPackOne a r of
+            Just r' -> Just (r' : rs)
+            Nothing -> (r :) `fmap` tryPack a rs
 
-    insertsMap :: [Location (Int, Int)] -> Map (Int, Int) [Location (Int, Int)] -> Map (Int, Int) [Location (Int, Int)]
-    insertsMap rects m = List.foldl' (\n rect -> Map.insertWith (++) (value rect) [rect] n) m rects
+    tryPackOne :: (Int, (Int, Int)) -> Rect Int -> Maybe (Rect Int)
 
-    third (_, _, a) = a
+    tryPackOne a @ (index, (w, h)) r @ (Rect _ (rw, rh) Nothing) =
+        if rw < w || rh < h
+            then Nothing
+            else Just $ rect (position r) (size r) a
+
+    tryPackOne a @ (index, (w, h)) r @ (Rect _ (rw, rh) (Just (e, childR, childB))) =
+        if rw < w || rh < h
+            then Nothing
+            else
+                tryPackOne a childR >>= \childR' -> Just r { element = Just (e, childR', childB) }
+                `mplus`
+                tryPackOne a childB >>= \childB' -> Just r { element = Just (e, childR, childB') }
+                
 
 
+
+{-
 writeTexture :: FilePath -> (Int, Int) -> [Location Int] -> Array Int Picture.DynamicImage -> IO ()
 writeTexture destination (width, height) locations sources =
     do
@@ -117,3 +119,4 @@ writeTexture destination (width, height) locations sources =
                 (\(x, y, a) -> Picture.writePixel texture (ox + x) (oy + y) a)
                 [(x, y, Picture.pixelAt img' x y) | x <- [0..(w - 1)], y <- [0..(h - 1)]]
 
+-}
